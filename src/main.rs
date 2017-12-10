124625a8515593e2b9eafc26b36fd11242f8fd70
Luke Bluett <Luke.J.Bluett@gmail.com>
2017-12-10 21:52:51 +0000

    Gladefile displays

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+extern crate gtk;
+use gtk::prelude::*;
+
+fn main() 
+{
+    gtk::init();
+
+    let glade_src = include_str!("../res/file_explorer.glade");
+
+    let builder = gtk::Builder::new_from_string(glade_src);
+
+    let window : gtk::Window = builder.get_object("windowMain").unwrap();
+
+    window.connect_delete_event
+    (|_, _|
+        {
+            gtk::main_quit();
+            gtk::Inhibit(true)
+        }
+    );
+
+    window.show_all();
+        
+    gtk::main();
 }
